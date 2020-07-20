ddb57c36d6d07f9fd39fc965752b2161182c5a5a
papaps <45527966+papaps@users.noreply.github.com>
2020-07-20 21:50:28 +0800

    Fixed files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const functions = {
   gradeClassifier: (grade) => {
     // TODO: Write code here
+    if (grade >= 95.00 && grade <= 100.00)
+      return("O - Outstanding")
+    else if (grade >= 90.00 && grade <= 94.99)
+      return("V - Very Good")
+    else if (grade >= 85.00 && grade <= 89.99)
+      return("G - Good")
+    else if (grade >= 80.00 && grade <= 84.99)
+      return("S - Satisfactory")
+    else if (grade >= 75.00 && grade <= 79.99)
+      return("N - Needs Improvement")
+    else if (grade <= 74.99)
+      return("D - Did not Meet Expectation")
   }
 }
 
--- src/index.test.js
+++ src/index.test.js
@@ -2,11 +2,57 @@ const { gradeClassifier } = require('./index');
 
 // TODO: Write unit tests here
 describe('Scenario', () => {
-  it('When ..., then ...', () => {
+  it("When student's grades is equal to or between 95 to 100, then grade should be O", () => {
     // Arrange
-
+      const grade = 100;
     // Act
-
+      const receivedGrade = gradeClassifier(grade);
     // Assert
+      expect(receivedGrade).toMatch("O - Outstanding");
+  });
+
+  it("When student's grades is equal to or between 90 to 94.99, then grade should be V", () => {
+    // Arrange
+        const grade = 92.99;
+    // Act
+        const receivedGrade = gradeClassifier(grade);
+    // Assert
+        expect(receivedGrade).toMatch("V - Very Good");
+  });
+
+  it("When student's grades is equal to or between 85 to 89.99, then grade should be G", () => {
+    // Arrange
+        const grade = 89.98;
+    // Act
+        const receivedGrade = gradeClassifier(grade);
+    // Assert
+        expect(receivedGrade).toMatch("G - Good");
+  });
+
+  it("When student's grades is equal to or between 80 to 84.99, then grade should be S", () => {
+        // Arrange
+        const grade = 80;
+    // Act
+    const receivedGrade = gradeClassifier(grade);
+    // Assert
+    expect(receivedGrade).toMatch("S - Satisfactory");
+  });
+
+  it("When student's grades is equal to or between 75 to 79.99, then grade should be N", () => {
+        // Arrange
+        const grade = 75.01;
+    // Act
+    const receivedGrade = gradeClassifier(grade);
+    // Assert
+    expect(receivedGrade).toMatch("N - Needs Improvement");
+  });
+
+  it("When student's grades is equal to or less than 74.99, then grade should be D", () => {
+        // Arrange
+        const grade = 50;
+    // Act
+    const receivedGrade = gradeClassifier(grade);
+    // Assert
+    expect(receivedGrade).toMatch("D - Did not Meet Expectation");
   });
 });
